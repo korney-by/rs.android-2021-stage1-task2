8478d214bd25085c1a1de9cf6e414e8815183161
Sergey Kornienko <korney.by@gmail.com>
2021-06-10 01:44:46 +0300

    feat: Abbreviation is done

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,30 @@
 package subtask2
 
+fun main(){
+    val converter = Abbreviation()
+    print(converter.abbreviationFromA("daBcd", "ABC"))
+}
+
 class Abbreviation {
 
     // TODO: Complete the following function
-    fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+    fun abbreviationFromA(a: String, b: String):String {
+        //throw NotImplementedError("Not implemented")
+
+        var lastFindNumber: Int = -1
+        var currFindNumber: Int
+        for (i in 0 until b.length) {
+            currFindNumber = a.indexOf(b[i], 0, true)
+            if (currFindNumber <= lastFindNumber) {
+                return ANSWER_NO
+            }
+            lastFindNumber=currFindNumber
+        }
+        return ANSWER_YES
+    }
+
+    companion object{
+        val ANSWER_YES="YES"
+        val ANSWER_NO="NO"
     }
 }
