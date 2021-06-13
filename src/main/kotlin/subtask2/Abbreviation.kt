2ff38298903ca75538f2e7da8a2a6dd0a4f18647
Sergey Kornienko <korney.by@gmail.com>
2021-06-13 19:54:00 +0300

    feat: for Abbreviation added another variant solve, and optimized existing one

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,30 +1,42 @@
 package subtask2
 
-fun main(){
+fun main() {
     val converter = Abbreviation()
-    print(converter.abbreviationFromA("daBcd", "ABC"))
+    println(converter.abbreviationFromA("dacBd", "ABC"))
+    println(converter.abbreviationFromA2("dacBacd", "ABC"))
 }
 
 class Abbreviation {
 
     // TODO: Complete the following function
-    fun abbreviationFromA(a: String, b: String):String {
+
+    fun abbreviationFromA2(a: String, b: String): String {
+        var lastPosition = -1
+        return if (b.filter {
+                lastPosition = a.indexOf(it, lastPosition + 1, true)
+                lastPosition >= 0
+            }.contains(b)) {
+            ANSWER_YES
+        } else {
+            ANSWER_NO
+        }
+    }
+
+    fun abbreviationFromA(a: String, b: String): String {
         //throw NotImplementedError("Not implemented")
 
         var lastFindNumber: Int = -1
-        var currFindNumber: Int
         for (element in b) {
-            currFindNumber = a.indexOf(element, 0, true)
-            if (currFindNumber <= lastFindNumber) {
+            lastFindNumber = a.indexOf(element, lastFindNumber + 1, true)
+            if (lastFindNumber == -1) {
                 return ANSWER_NO
             }
-            lastFindNumber=currFindNumber
         }
         return ANSWER_YES
     }
 
-    companion object{
-        const val ANSWER_YES="YES"
-        const val ANSWER_NO="NO"
+    companion object {
+        const val ANSWER_YES = "YES"
+        const val ANSWER_NO = "NO"
     }
 }
